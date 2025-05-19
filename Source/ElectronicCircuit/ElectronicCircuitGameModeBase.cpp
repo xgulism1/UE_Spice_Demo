3fcfadb29c35e39377d1a565c2ba7f258915ff0b
Martin Gulis <gulis.martin@gmail.com>
2025-05-19 14:18:57 +0200

    Updated to new version

--- Source/ElectronicCircuit/ElectronicCircuitGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (C) 2022-2024 Martin Gulis. All Rights Reserved.
-
-
-#include "ElectronicCircuitGameModeBase.h"
-
--- Source/ElectronicCircuitEditor.Target.cs
+++ Source/ElectronicCircuitEditor.Target.cs
@@ -1,15 +1,14 @@
-// Copyright (C) 2022-2024 Martin Gulis. All Rights Reserved.
+// Copyright (C) 2022-2025 Martin Gulis. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class ElectronicCircuitEditorTarget : TargetRules
 {
-	public ElectronicCircuitEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V4;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
-        ExtraModuleNames.AddRange( new string[] { "ElectronicCircuit" } );
-	}
+    public ElectronicCircuitEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.AddRange(new string[] { "ElectronicCircuit" });
+    }
 }
